601985608f7de718f146fb49105333fe951e43a8
LakhderAmine99 <amine.lakhder@usmba.ac.ma>
2022-10-27 13:27:08 +0100

    setup files

---
+++ src/modules/fruitsDataProcessor.py
@@ -0,0 +1,17 @@
+
+
+def _decode(name):
+    return
+
+def _load_files():
+    return 
+
+def load_data():  
+    
+    X_train = []
+    y_train = []
+    
+    X_test = []
+    y_test = []
+    
+    return (X_train,y_train),(X_test,y_test)
